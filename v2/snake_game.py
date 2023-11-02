323e34242b66e1a1e7144f5c23d023dbbfe21280
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2023-11-02 15:15:08 -0400

    Moved Snake to its own file

--- v2/snake_game.py
+++ v2/snake_game.py
@@ -5,6 +5,7 @@ import random
 from enum import Enum
 from collections import namedtuple
 from helper import AMOUNT_OF_FRAMES_TO_DEATH_MULTIPLIER
+from Snake import Snake
 import numpy as np
 
 pygame.init()
@@ -29,87 +30,8 @@ BLACK = (0,0,0)
 BLOCK_SIZE = 20
 SPEED = 150
 
-
-
 WIDTH = 640
 HEIGHT = 480
-
-class Snake:
-    def __init__(self, direction, head, snake, score, food, gameOver, model):
-        self.direction = direction
-        self.head = head
-        self.snake = snake
-        self.score = score
-        self.food = food
-        self.gameOver = gameOver
-        self.model = model
-        self.frameIterations = 0
-        # Used to keep track of how it died
-        # 0 Lazy, didn't get any more fruit and just died bc of multiplier death
-        # 1 Wall
-        # 2 Hit itself
-        self.death = None
-        self.finalLength = None
-    
-    def getDirection(self):
-        return self.direction
-
-    def setDirection(self, newDir):
-        self.direction = newDir
-
-    def getHead(self):
-        return self.head
-
-    def setHead(self, newHead):
-        self.head = newHead
-
-    def getSnake(self):
-        return self.snake
-
-    def setSnake(self, newSnake):
-        self.snake = newSnake
-
-    def getScore(self):
-        return self.score
-
-    def setScore(self, newScore):
-        self.score = newScore
-
-    def getFood(self):
-        return self.food
-
-    def setFood(self, newFood):
-        self.food = newFood
-    
-    def getGameOver(self):
-        return self.gameOver
-
-    def setGameOver(self, newGameOver):
-        self.gameOver = newGameOver
-
-    def getModel(self):
-        return self.model
-
-    def setModel(self, newModel):
-        self.model = newModel
-
-    def getFrameIterations(self):
-        return self.frameIterations
-
-    def setFrameIterations(self, newIterations):
-        self.frameIterations = newIterations
-    
-    def getDeath(self):
-        return self.death
-    
-    def setDeath(self, newDeath):
-        self.death = newDeath
-    
-    def getFinalLength(self):
-        return self.finalLength
-
-    def setFinalLength(self, newFinalLength):
-        self.finalLength = newFinalLength
 
 class SnakeGameAI:
     def __init__(self, numSnakes, w=WIDTH, h=HEIGHT):
